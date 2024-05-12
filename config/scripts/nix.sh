a90b7be7f0a886497027d402a325ed870b8d2c69
Meptl <git-client@meptl.com>
2024-05-12 02:29:21 -0400

    Use default nix script. Disable yafti

--- config/scripts/nix.sh
+++ config/scripts/nix.sh
@@ -3,11 +3,12 @@
 set -oue pipefail
 
 # Install nix.
-curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --no-confirm
-nix-channel --add https://nixos.org/channels/nixpkgs-unstable
-nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
-nix-channel --update
+sh <(curl -L https://nixos.org/nix/install) --daemon
+# curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install ostree --no-confirm
+# nix-channel --add https://nixos.org/channels/nixpkgs-unstable
+# nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+# nix-channel --update
 
-# Install home-manager
-nix-shell '<home-manager>' -A install
-# home-manager switch -b backup
+# # Install home-manager
+# nix-shell '<home-manager>' -A install
+# # home-manager switch -b backup
